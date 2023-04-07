a4907584f35c65da91553a3a12e216b1f509f9d7
Andrew Vancil <andrew.vancil@cchmc.org>
2023-04-07 10:55:55 -0400

    update address selection; base pipes

--- monthly_data_report.R
+++ monthly_data_report.R
@@ -1,27 +1,31 @@
-# # # # #test file
-# d <- read_csv('test/simulated_jfs_data_geocoded_2021.csv',
+# # # #test file
+# d <- read_csv('test/sim_test_data_new_prepped_geocoder_3.3.0_score_threshold_0.5_census_block_group_0.6.0_2010.csv',
 #               col_types = cols(INTAKE_ID = col_character(),
-#                                SCREENING_DECISION = col_character(),
-#                                #DECISION_DATE = col_character(),
-#                                DECISION_DATE = col_date(format = "%m/%d/%Y"),
-#                                PERSON_ID = col_character(),
-#                                RACE = col_character(),
-#                                ADDRESS_START = col_date(),
-#                                MANDATED_REPORTER = col_character(),
-#                                #  REPORTER_PERSON_ID = col_character(),
-#                                address_type = col_character(),
-#                                address = col_character(),
-#                                bad_address = col_logical(),
-#                                PO = col_logical(),
-#                                lat = col_double(),
-#                                lon = col_double(),
-#                                score = col_double(),
-#                                precision = col_character(),
-#                                precise_geocode = col_logical(),
-#                                census_block_group_id_2010 = col_character(),
-#                                census_tract_id_2010 = col_character(),
-#                                BIRTH_DATE = col_date(format = "%m/%d/%Y"),
+#                                                 SCREENING_DECISION = col_character(),
+#                                                 DECISION_DATE = col_date(format = "%m/%d/%y"),#format = "%m/%d/%Y"),
+#                                                 BIRTH_DATE = col_date(format = "%m/%d/%y"),#format = "%m/%d/%Y"),
+#                                                 PERSON_ID = col_character(),
+#                                                 RACE = col_character(),
+#                                                 ADDRESS_START = col_date(format = "%m/%d/%y"),
+#                                                 REPORTER_PERSON_ID = col_character(),
+#                                                 MANDATED_REPORTER = col_character(),
+#                                                 address_type = col_character(),
+#                                                 address = col_character(),
+#                                                 bad_address = col_logical(),
+#                                                 PO = col_logical(),
+#                                                 matched_street = col_character(),
+#                                                 matched_zip = col_character(),
+#                                                 matched_city = col_character(),
+#                                                 matched_state = col_character(),
+#                                                 lat = col_double(),
+#                                                 lon = col_double(),
+#                                                 score = col_double(),
+#                                                 precision = col_character(),
+#                                                 geocode_result = col_character(),
+#                                                 census_block_group_id_2010 = col_character(),
+#                                                 census_tract_id_2010 = col_character()
 #               ))
+
 library(tidyr)
 library(readr)
 library(dplyr)
@@ -37,27 +41,40 @@ options(dplyr.summarise.inform = FALSE)
 ##
 
 # consider 'SCREENED IN AR' same as 'SCREENED IN'
-d <- d %>%
+d <- d |>
   mutate(screened_in = SCREENING_DECISION %in% c("SCREENED IN", "SCREENED IN AR"))
 
+#if two different addresses, keep allegation; if one address, keep whichever it is
+d1 <- d |> 
+  group_by(INTAKE_ID) |> 
+  filter(n() == 1) |> 
+  ungroup()
 
-d <- d %>%
-  filter(!duplicated(INTAKE_ID)) %>%
-  select( -ADDRESS_START, -address_type) %>%
+d2 <- d |>
+  group_by(INTAKE_ID) |> 
+  filter(n() == 2) |> 
+  filter(address_type == 'ALLEGATION_ADDRESS') |> 
+  ungroup()
+
+d <- rbind(d1, d2)
+
+##
+d <- d  |> 
+  filter(!duplicated(INTAKE_ID)) |>
   mutate(month = lubridate::month(DECISION_DATE),
          year = lubridate::year(DECISION_DATE)) 
 
 #get age, sort into groups
-d <- d %>%
-  mutate(BIRTH_DATE = replace_na(BIRTH_DATE, as.Date('1900-01-01'))) %>%
-  mutate(age = as.numeric(difftime(DECISION_DATE, BIRTH_DATE, units = "weeks")/52.25)) %>%
+d <- d |>
+  mutate(BIRTH_DATE = replace_na(BIRTH_DATE, as.Date('1900-01-01'))) |>
+  mutate(age = as.numeric(difftime(DECISION_DATE, BIRTH_DATE, units = "weeks")/52.25)) |>
   mutate(age_grp = case_when(age <= 5.0 ~ "zero_five",
                              age < 19 ~ "five_plus",
                              age >= 19 ~ "no_birth_date"))
           
 ##Annual counts
-d_yearly <- d %>%
-  group_by(year) %>%
+d_yearly <- d |>
+  group_by(year) |>
   summarize(num_year = n(), .groups = "keep")
 
 print(knitr::kable(d_yearly))
@@ -65,62 +82,62 @@ print(knitr::kable(d_yearly))
 ##
 
 ### Monthly Counts
-d_neigh <- d  %>%
-  mutate(fips_tract_id = as.character(census_tract_id_2010)) %>% #comment these out when testing with concentrated data
-  left_join(tract_to_neighborhood, by='fips_tract_id') %>%
+d_neigh <- d  |>
+  mutate(fips_tract_id = as.character(census_tract_id_2010)) |> #comment these out when testing with concentrated data
+  left_join(tract_to_neighborhood, by='fips_tract_id') |>
   filter(!is.na(DECISION_DATE))
 
-d_neigh <- d_neigh %>%
+d_neigh <- d_neigh |>
   mutate(neighborhood = ifelse(is.na(lat), 'Missing', neighborhood))
 
-screen_neighborhood_age <- d_neigh %>%
-  group_by(neighborhood, year, month, age_grp) %>%
+screen_neighborhood_age <- d_neigh |>
+  group_by(neighborhood, year, month, age_grp) |>
   summarise(n_screened_in = sum(screened_in == TRUE, na.rm = TRUE),
             n_calls = n(),
             .groups = "keep"
-            ) %>%
+            ) |>
   ungroup() 
 
-screen_neighborhood_age_2 <- screen_neighborhood_age %>% 
-  group_by(year, month, age_grp) %>%
+screen_neighborhood_age_2 <- screen_neighborhood_age |> 
+  group_by(year, month, age_grp) |>
   summarise(neighborhood = "Total",
             n_calls = sum(n_calls),
             n_screened_in = sum(n_screened_in)) %>%
   bind_rows(screen_neighborhood_age, .)
 
 
-screen_neighborhood_age_2 <- screen_neighborhood_age_2 %>%
+screen_neighborhood_age_2 <- screen_neighborhood_age_2 |>
   rename(group = "age_grp")
 
 
-screen_neighborhood_MR <- d_neigh %>%
-  group_by(neighborhood, year, month, MANDATED_REPORTER) %>%
+screen_neighborhood_MR <- d_neigh |>
+  group_by(neighborhood, year, month, MANDATED_REPORTER) |>
   summarise(n_screened_in = sum(screened_in == TRUE, na.rm = TRUE),
             n_calls = n(),
             .groups = "keep"
-  ) %>%
+  ) |>
   ungroup() 
 
-screen_neighborhood_MR_2 <- screen_neighborhood_MR %>% 
-  group_by(year, month, MANDATED_REPORTER) %>%
+screen_neighborhood_MR_2 <- screen_neighborhood_MR |> 
+  group_by(year, month, MANDATED_REPORTER) |>
   summarise(neighborhood = "Total",
             n_calls = sum(n_calls),
             n_screened_in = sum(n_screened_in)) %>%
   bind_rows(screen_neighborhood_MR, .)
 
-screen_neighborhood_MR_2 <- screen_neighborhood_MR_2 %>%
-  mutate(MANDATED_REPORTER = recode_factor(MANDATED_REPORTER, Yes = "MR", No = "Non_MR")) %>%
+screen_neighborhood_MR_2 <- screen_neighborhood_MR_2 |>
+  mutate(MANDATED_REPORTER = recode_factor(MANDATED_REPORTER, Yes = "MR", No = "Non_MR")) |>
   rename(group = "MANDATED_REPORTER")
 
-screen_neighborhood <- rbind(screen_neighborhood_age_2, screen_neighborhood_MR_2) %>%
+screen_neighborhood <- rbind(screen_neighborhood_age_2, screen_neighborhood_MR_2) |>
   arrange(neighborhood, year, month)
 
-screen_neighborhood_2 <- screen_neighborhood %>%
-  group_by(neighborhood, year, month) %>%
+screen_neighborhood_2 <- screen_neighborhood |>
+  group_by(neighborhood, year, month) |>
   summarise(group = "Total",
             n_calls = sum(n_calls),
             n_screened_in = sum(n_screened_in)) %>%
-  bind_rows(screen_neighborhood, .) %>%
+  bind_rows(screen_neighborhood, .) |>
   arrange(neighborhood, year, month)
 
 
@@ -138,29 +155,29 @@ screen_neighborhood_2 <- screen_neighborhood %>%
 #             n_screened_in_NON_MR = sum(screened_in == TRUE & MANDATED_REPORTER == 'No', na.rm = TRUE),
 #             n_calls_NON_MR = length(INTAKE_ID[MANDATED_REPORTER == 'No']),
 #             .groups = "keep"
-#             ) %>%
-#   ungroup() %>% 
-#   group_by(year, two_week) %>%
+#             ) |>
+#   ungroup() |> 
+#   group_by(year, two_week) |>
 #   bind_rows(summarize(., across(where(is.numeric), sum),
 #                       across(where(is.character), ~"Total"), .groups = "keep"))
 #   
 # 
-# screen_neighborhood_rate <- screen_neighborhood %>%
-#   group_by(neighborhood, year, two_week) %>%
+# screen_neighborhood_rate <- screen_neighborhood |>
+#   group_by(neighborhood, year, two_week) |>
 #   mutate(screen_in_rate_all = round(n_screened_in_all/n_calls_all,2),
 #          screen_in_rate_less_5 = round(n_screened_in_less_5/n_calls_less_5,2),
 #          screen_in_rate_greater_5 = round(n_screened_in_greater_5/n_calls_greater_5,2),
 #          screen_in_rate_no_bd = round(n_screened_in_no_bd/n_calls_no_bd,2),
 #          screen_in_rate_MR = round(n_screened_in_MR/n_calls_MR,2),
 #          screen_in_rate_NON_MR = round(n_screened_in_NON_MR/n_calls_NON_MR,2)
-#          ) %>%
+#          ) |>
 #   mutate(across(starts_with('n_'),
 #             ~ifelse(.x < 5, NA, .x)))
 ##---
 
-screen_neighborhood_rate <- screen_neighborhood_2 %>%
-  rowwise() %>%
-  mutate(screen_in_rate = round(n_screened_in / n_calls, 2)) %>%
+screen_neighborhood_rate <- screen_neighborhood_2 |>
+  rowwise() |>
+  mutate(screen_in_rate = round(n_screened_in / n_calls, 2)) |>
   mutate(across(starts_with('n_'),
                 ~ifelse(.x < 5, NA, .x)))
 
@@ -169,12 +186,12 @@ screen_neighborhood_rate <- screen_neighborhood_2 %>%
 ##
 d_csv <- screen_neighborhood_rate
 
-# d_csv <- screen_neighborhood_rate %>%
+# d_csv <- screen_neighborhood_rate |>
 #   select(neighborhood, year, two_week,
 #          number_of_calls = n_calls,
 #          number_of_calls_screened_in = n_screened_in,
 #          screen_in_rate = screen_in_rate)
 
 path <- "/tmp/"
-write.csv(d_csv, paste(path, "monthly_report_v5.0.0.csv", sep = ''))
+write.csv(d_csv, paste(path, "monthly_report_v5.0.1.csv", sep = ''))
 
